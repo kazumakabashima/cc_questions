121f133649a6367951c1f52dab5c30cc4cc0fad2
kazuma kabashima <126457732+kazumakabashima@users.noreply.github.com>
2023-09-23 11:02:26 +0900

    Delete TokyoCallCenterApp directory

--- TokyoCallCenterApp/JavaScript/answerInput.js
+++
@@ -1,6 +0,0 @@
-const answerInput = (question,elements) => {
-    elements.elementsWithAnswer.innerHTML = question.answer;
-    elements.elementsWithDescription.innerHTML = question.description;
-};
-
-export default answerInput;
