eece4771d6a972601deb6b31a15eaba3b95e8320
kazuma kabashima <126457732+kazumakabashima@users.noreply.github.com>
2023-09-25 18:59:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,64 @@
-import getApiData from './JavaScript/api.js';
-import getElements from './JavaScript/getElement.js';
-import changeElements from './JavaScript/changeElement.js';
-import answerInput from './JavaScript/answerInput.js';
+<!DOCTYPE html>
+<html><!-- from index.html -->
+    <head><!-- from head -->
+        <!-- metadata -->
+        <meta charset="utf-8">
+        <title>TokyoCallCenterApp</title>
 
-// getQuestions 関数を questionPreference 関数内に移動
-const questionPreference = async () => {
-    try {
-        // getApiData関数を非同期で呼び出す
-        const questions = await getQuestions();
-        // 必要な要素を取得する
-        const elements = getElements();
+        <!-- import -->
+        <script src="index.js" type="module"></script>
+        <link rel="stylesheet" href="css/style.css">
 
-        let i = 0;
+        <!-- google font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=M+PLUS+1p&family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
 
-        // 要素に初期問題を設定する
-        changeElements(questions[i], elements);
-        elements.elementsWithQuestionCount.innerHTML = `${i + 1}/${questions.length}`;
+    </head><!-- upto head -->
+    
+    <body><!-- from body -->
+        <header>
+            <div class="header">
+                <p>TOKYO CALL CENTER</p>
+                <nav>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        
+        <main>
+            <div class="hero-container">
+                <section>
+                    <div class="hero-contents">
+                        <h1 class="hero-text">練習問題</h1>
+                        <p class="hero-list-title">操作方法</p>
+                        <ul>
+                            <li>座席番号を入力</li>
+                            <li>出題数を選択</li>
+                            <li>問題を解くをクリック</li>
+                            <li>解答にチェックを入れて答えを確認する</li>
+                            <li>次の問題をクリック</li>
+                            <li>選択出題数の問題が出題されて終了</li>
+                        </ul>
+                        <div class="hero-forms">
+                            <input type="text" id="seat-num" placeholder="座席番号を入力" class="hero-form input-form">
+                            <select id="question_count" class="hero-form select-form">
+                                <option value="" disabled selected style="display:none;">出題数を選択</option>
+                                <option value="10">10</option>
+                            </select>
+                            <button type="button" id="stat-btn" class="hero-form stat-btn">問題を解く</button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
 
-        elements.elementsWithIsAnswer.addEventListener('click', function() {
-            answerInput(questions[i],elements);
-        });
-
-        elements.elementsWithBackButton.addEventListener('click', function() {
-            // インデックスが最初の問題に達した場合は最初の問題に戻る
-            if (i === 0) {
-                alert("現在の問題が、最初の問題です！");
-            } else {
-                elements.elementsWithNextButton.innerHTML = "次へ"
-                i--;
-                const question = questions[i];
-                changeElements(question, elements);
-                elements.elementsWithQuestionCount.innerHTML = `${i+ 1}/${questions.length}`;
-            };
-        });
-
-        elements.elementsWithNextButton.addEventListener('click', function() {
-            // インデックスが最後の問題に達した場合は最初の問題に戻る
-            if (i === questions.length - 2) {
-                elements.elementsWithNextButton.innerHTML = "トップに戻る";
-            };
-
-            if(i < questions.length - 1) {
-                i++;
-                const question = questions[i];
-                changeElements(question, elements);
-                elements.elementsWithQuestionCount.innerHTML = `${i + 1}/${questions.length}`;
-            } else {
-                window.location.href = "index.html";
-            }
-        });
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-// getQuestions 関数を questionPreference 関数の前に定義
-const getQuestions = async () => {
-    try {
-        // getApiData関数を非同期で呼び出す
-        const questions = await getApiData();
-        // データを取得した後に他の処理を行う
-        console.log(questions);
-        // 他の処理を記述
-        return questions; // 取得したデータを返す
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-window.addEventListener('load', questionPreference);
+        <footer>
+            <div class="footer">
+                <p>Version 1.0.0 -- Released on September 23, 2023</p>
+            </div>
+        </footer>
+    </body><!-- upto body -->
+</html><!-- upto index.html -->
