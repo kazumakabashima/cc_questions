f0ceb6ce1a03774822d0aae1aef2559f0e856b4f
kazuma kabashima <126457732+kazumakabashima@users.noreply.github.com>
2023-09-25 19:08:14 +0900

    Update getElement.js

--- JavaScript/getElement.js
+++ JavaScript/getElement.js
@@ -1,30 +1,40 @@
 const getElements = () => {
-    const elementsWithQuestionId = document.getElementById('question_id');
-    const elementsWithQuestionText = document.getElementById('question_text');
-    const elementsWithSelectionA = document.getElementById('selectionA');
-    const elementsWithSelectionB = document.getElementById('selectionB');
-    const elementsWithSelectionC = document.getElementById('selectionC');
-    const elementsWithSelectionD = document.getElementById('selectionD');
+    const elementsWithQuestionNumber = document.getElementById('question-number');
+    const elementsWithQuestionText = document.getElementById('question-text');
+    const elementsWithAnswerLabel01 = document.getElementById('answer-label01');
+    const elementsWithAnswerLabel02 = document.getElementById('answer-label02');
+    const elementsWithAnswerLabel03 = document.getElementById('answer-label03');
+    const elementsWithAnswerLabel04 = document.getElementById('answer-label04');
+    const elementsWithAnswer01 = document.getElementById('answer01');
+    const elementsWithAnswer02 = document.getElementById('answer02');
+    const elementsWithAnswer03= document.getElementById('answer03');
+    const elementsWithAnswer04 = document.getElementById('answer04');
     const elementsWithIsAnswer = document.getElementById('isAnswer');
+    const elementsWithAnswerValid = document.getElementById('answer-valid')
     const elementsWithAnswer = document.getElementById('answer');
     const elementsWithDescription = document.getElementById('description');
     const elementsWithNextButton = document.getElementById('next_btn');
     const elementsWithBackButton = document.getElementById('back_btn');
-    const elementsWithQuestionCount = document.getElementById('question_count');
+    const elementsWithQuestionId = document.getElementById('question_id');
     
     return {
-        elementsWithQuestionId,
+        elementsWithQuestionNumber,
         elementsWithQuestionText,
-        elementsWithSelectionA,
-        elementsWithSelectionB,
-        elementsWithSelectionC,
-        elementsWithSelectionD,
+        elementsWithAnswerLabel01,
+        elementsWithAnswerLabel02,
+        elementsWithAnswerLabel03,
+        elementsWithAnswerLabel04,
+        elementsWithAnswer01,
+        elementsWithAnswer02,
+        elementsWithAnswer03,
+        elementsWithAnswer04,
         elementsWithIsAnswer,
+        elementsWithAnswerValid,
         elementsWithAnswer,
         elementsWithDescription,
         elementsWithNextButton,
         elementsWithBackButton,
-        elementsWithQuestionCount,
+        elementsWithQuestionId,
     };
 };
 
