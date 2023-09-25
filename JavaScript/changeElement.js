08a36d24910e26e4e314ab45ee147e66165e2582
kazuma kabashima <126457732+kazumakabashima@users.noreply.github.com>
2023-09-25 19:07:56 +0900

    Update changeElement.js

--- JavaScript/changeElement.js
+++ JavaScript/changeElement.js
@@ -1,12 +1,45 @@
-const changeElements = (question, elements) => {
-    elements.elementsWithQuestionId.innerHTML = question.id;
+import shuffleArray from "./shuffle.js";
+import getAnswersObject from "./getAnswersObject.js";
+import changeStyleVisibility from "./visibility.js";
+import scroll from "./scroll.js";
+
+const changeElements = (i, questionCount, question, elements) => {
+    //解答選択肢をランダムに並べ替えて解答を取得する。
+    const answer = question.answer;
+    const answersObject = getAnswersObject(question);
+    shuffleArray(answersObject);
+
+    const questionNumber = `Q ${i + 1}/${questionCount}`;
+
+    elements.elementsWithQuestionNumber.innerHTML = questionNumber;
     elements.elementsWithQuestionText.innerHTML = question.question;
-    elements.elementsWithSelectionA.innerHTML = question.selectionA;
-    elements.elementsWithSelectionB.innerHTML = question.selectionB;
-    elements.elementsWithSelectionC.innerHTML = question.selectionC;
-    elements.elementsWithSelectionD.innerHTML = question.selectionD;
-    elements.elementsWithAnswer.innerHTML = "";
-    elements.elementsWithDescription.innerHTML = "";
-} 
+
+    elements.elementsWithAnswerLabel01.textContent = answersObject[0];
+    elements.elementsWithAnswerLabel02.textContent = answersObject[1];
+    elements.elementsWithAnswerLabel03.textContent = answersObject[2];
+    elements.elementsWithAnswerLabel04.textContent = answersObject[3];
+
+    elements.elementsWithAnswer01.value = answersObject[0];
+    elements.elementsWithAnswer02.value = answersObject[1];
+    elements.elementsWithAnswer03.value = answersObject[2];
+    elements.elementsWithAnswer04.value = answersObject[3];
+
+    elements.elementsWithAnswer.innerHTML = answer;
+    elements.elementsWithDescription.innerHTML = question.description;
+    elements.elementsWithQuestionId.innerHTML = `ID: ${question.id}`;
+
+    // ラジオボタングループ内のすべてのラジオボタン要素を取得
+    const radioButtons = document.querySelectorAll('input[type="radio"][name="answer-selection"]');
+
+    // ラジオボタンを非選択状態にする
+    radioButtons.forEach((radioButton) => {
+        radioButton.checked = false;
+    });
+
+    changeStyleVisibility('hidden');
+    scroll('work-container');
+};
 
 export default changeElements;
+
+
